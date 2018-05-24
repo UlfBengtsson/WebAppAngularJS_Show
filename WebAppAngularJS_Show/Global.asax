ec9db70dc72ede3407bda2e5dab1f99e0141d27c
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2018-05-24 10:13:31 +0200

    Add project files.

---
+++ WebAppAngularJS_Show/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppAngularJS_Show.MvcApplication" Language="C#" %>
---
+++ WebAppAngularJS_Show/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppAngularJS_Show.Startup))]
+namespace WebAppAngularJS_Show
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
